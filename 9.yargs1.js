cb175864c2a9969b944c2ac7b79ae32bfc7964aa
Noerkholizs <148823915+Noerkholizs@users.noreply.github.com>
2024-11-25 20:56:38 +0700

    menambah fitur list,detail,update,delete dan authentikasi data (aku sangat bingung, tapi jauh lebih baik...)

--- 9.yargs1.js
+++ 9.yargs1.js
@@ -1,6 +1,8 @@
 const yargs = require("yargs");
 const validator = require("validator");
+const func = require("./src/func");
 
+// ADD CONTACT
 yargs.command({
   command: "add",
   describe: "add new contact",
@@ -21,6 +23,7 @@ yargs.command({
       type: "string",
     },
   },
+
   handler(argv) {
     const contact = {
       name: argv.name,
@@ -43,9 +46,84 @@ yargs.command({
 
     if (isValid) {
       console.log(contact);
+      func.addContact(contact);
     } else {
       console.log(errorMessage);
     }
+  },
+});
+
+// UPDATE CONTACT
+yargs.command({
+  command: "update",
+  describe: "update contact data",
+  builder: {
+    oldName: {
+      describe: "existing contact name to update",
+      demandOption: true,
+      type: "string",
+    },
+    name: {
+      describe: "new contact name",
+      demandOption: false,
+      type: "string",
+    },
+    email: {
+      describe: "new contact email",
+      demandOption: false,
+      type: "string",
+    },
+    mobile: {
+      describe: "new contact mobile",
+      demandOption: false,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    func.updateContact(argv.oldName, argv.name, argv.email, argv.mobile);
+  },
+});
+
+// DELETE CONTACT
+yargs.command({
+  command: "delete",
+  describe: "delete contact",
+  builder: {
+    name: {
+      describe: "contact name",
+      demandOption: true,
+      type: "string",
+    },
+  },
+
+  handler(argv) {
+    func.deleteContact(argv);
+  },
+}),
+  // DETAIL CONTACT
+  yargs.command({
+    command: "detail",
+    describe: "detail contact by name",
+    builder: {
+      name: {
+        describe: "contact name",
+        demandOption: true,
+        type: "string",
+      },
+    },
+
+    handler(argv) {
+      func.detailContact(argv.name); // Panggil fungsi showList dengan nama yang diteruskan dari argv
+    },
+  });
+
+// LIST CONTACT
+yargs.command({
+  command: "list",
+  describe: "list contact by name",
+
+  handler() {
+    func.listContact(); // Panggil fungsi showList dengan nama yang diteruskan dari argv
   },
 });
 
--- src/func.js
+++ src/func.js
@@ -1,6 +1,8 @@
 const readline = require("readline"); // Import modul 'readline' untuk membaca input dari pengguna
 const validator = require("validator"); // Import modul 'validator' untuk validasi
 const fs = require("fs"); // Import modul 'fs' untuk operasi file
+const { argv } = require("process");
+const { log } = require("console");
 const dirPath = "./data"; // Menyimpan path direktori data
 const dataPath = "./data/contacts.json"; // Menyimpan path file contacts.json
 
@@ -18,17 +20,7 @@ if (!fs.existsSync(dirPath)) {
 // membuat file contacts.json apabila tidak ada
 if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
-  // fs.writeFile("./data/contacts.json", "[]", "utf-8", (err) => {
-  //   if (err) throw err;
-  // });
 }
-
-//BELUM BERHASIL UNTUK MEMBUAT MENJADI SYNC
-// const question = (questions, callback) => {
-//   rl.question(questions, (input) => {
-//     callback(input);
-//   });
-// };
 
 const question = (questions) => {
   return new Promise((resolve, rejects) => {
@@ -38,15 +30,122 @@ const question = (questions) => {
   });
 };
 
+// Fungsi untuk menambahkan conctact (ADD)
 const addContact = async (contact) => {
   const contacts = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+
+  const isDuplicate = contacts.some(
+    (existingContact) =>
+      existingContact.name.toLowerCase() === contact.name.toLowerCase()
+  );
+
+  if (isDuplicate) {
+    console.log(`kontak dengan nama ${contact.name} sudah ada`);
+    return;
+  }
+
   contacts.push(contact);
   saveContacts(contacts);
 };
 
+// Fungsi untuk memperbaharui contact (UPDATE)
+const updateContact = (oldName, newName, newEmail, newMobile) => {
+  const contacts = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+
+  // Cari indeks kontak berdasarkan nama lama
+  const contactIndex = contacts.findIndex(
+    (contact) => contact.name.toLowerCase() === oldName.toLowerCase()
+  );
+
+  if (contactIndex === -1) {
+    console.log(`Contact with name "${oldName}" not found.`);
+    return;
+  }
+
+  // Validasi email dan mobile jika diupdate
+  if (newEmail && !validator.isEmail(newEmail)) {
+    console.log("Invalid email format.");
+    return;
+  }
+
+  if (newMobile && !validator.isMobilePhone(newMobile, "any")) {
+    console.log("Invalid mobile number format.");
+    return;
+  }
+
+  // Update data kontak
+  contacts[contactIndex] = {
+    name: newName || contacts[contactIndex].name,
+    email: newEmail || contacts[contactIndex].email,
+    mobile: newMobile || contacts[contactIndex].mobile,
+  };
+
+  saveContacts(contacts);
+  console.log(`Contact "${oldName}" has been updated successfully.`);
+};
+
+// Fungsi untuk menyimpan contact (SAVE)
 const saveContacts = (contacts) => {
   fs.writeFileSync(dataPath, JSON.stringify(contacts));
   console.log("data added successfully!");
 };
 
-module.exports = { question, addContact, rl };
+// Fungsi untuk menghapus kontak (DELETE)
+const deleteContact = (argv) => {
+  const contacts = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+  const filteredContacts = contacts.filter(
+    (contact) => contact.name !== argv.name
+  );
+
+  if (contacts.length === filteredContacts.length) {
+    console.log(`Contact with name "${argv.name}" not found.`);
+    return contacts;
+  }
+  saveContacts(filteredContacts);
+  console.log(`Contact with name "${argv.name}" has been deleted.`);
+};
+
+// Fungsi untuk menunjukan detail dari setiap kontak (DETAIL)
+const detailContact = (name) => {
+  const contacts = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+
+  if (name) {
+    const contact = contacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (contact) {
+      console.log(`Contact found:`);
+      console.log(`Name: ${contact.name}`);
+      console.log(`Email: ${contact.email}`);
+      console.log(`Mobile: ${contact.mobile}`);
+      console.log("-----------------------------");
+    } else {
+      console.log(`No contact found with the name "${name}".`);
+    }
+  }
+};
+
+// List Contact
+const listContact = () => {
+  const contacts = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+  if (contacts.length === 0) {
+    console.log("No contact found yoo");
+    return contacts;
+  }
+
+  console.log("List of contact:");
+  contacts.forEach((contact, index) => {
+    console.log(`${index + 1}. ${contact.name}`);
+  });
+};
+
+// Export disini
+module.exports = {
+  question,
+  addContact,
+  deleteContact,
+  detailContact,
+  listContact,
+  updateContact,
+  rl,
+};
