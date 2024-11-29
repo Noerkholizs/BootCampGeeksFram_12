8f3f03732a6ad4d8cceb88b19d897bbd832fbb36
Noerkholizs <148823915+Noerkholizs@users.noreply.github.com>
2024-11-29 11:34:01 +0700

    original tanpa css

--- express.js
+++ express.js
@@ -1,17 +1,28 @@
+const { name } = require("ejs");
 const express = require("express");
 const app = express();
 const port = 3000;
+const dataPath = "./data/contacts.json"; // Menyimpan path file contacts.json
+const func = require("./src/func");
+const fs = require("fs");
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  //res.sendFile(__dirname + "/views/index.html");
+  const name = "Majid";
+  res.render("index", { name });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
+  //res.sendFile(__dirname + "/views/about.html");
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(__dirname + "/views/contact.html");
+  //res.sendFile(__dirname + "/views/contact.html");
+  const contacts = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+  res.render("contact", { contacts });
 });
 
 app.use((req, res) => {
